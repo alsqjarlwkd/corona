e99e78920d96c660b1f272226af9ca6e541900b5
MinBeomgi <alsqjarlwkd@naver.com>
2021-07-08 23:14:49 +0900

    combobox value 값에 따라 컴포넌트 보여지는 부분 다르게 수정
    combobox value 값에 따라 컴포넌트 보여지는 부분 다르게 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React,{useEffect,useState} from "react";
-import Header from './components/header';
 import Contents from './components/contents';
 import GlobalContents from './components/GlobalContents';
 import UsaContents from './components/UsaContents';
@@ -10,6 +9,15 @@ import Modal from './components/Modal';
 
 
 function App() {
+  const[Value,setValue]=useState("")
+  const[component,setcomponent]=useState(
+    <>
+    <Contents></Contents>
+    <GlobalContents></GlobalContents>
+    <UsaContents></UsaContents>
+    <UkContents></UkContents>
+    </>
+  );
   useEffect(() => {
     const bgMode = window.localStorage.getItem("bgMode");
     if (bgMode === "dark") {
@@ -26,6 +34,38 @@ function App() {
       window.localStorage.setItem("bgMode", "dark");
     }
   }
+  useEffect(()=>{
+    if(Value === "kr")
+    {
+      return setcomponent(()=><Contents></Contents>)
+    }
+    else if(Value === "global")
+    {
+      return setcomponent(()=><GlobalContents></GlobalContents>)
+    }
+    else if(Value === "usa")
+    {
+      return setcomponent(()=><UsaContents></UsaContents>)
+    }
+    else if(Value === "uk")
+    {
+      return setcomponent(()=><UkContents></UkContents>)
+    }
+    else if(Value === "all")
+    {
+      return setcomponent(()=>{
+        return (
+          <>
+            <Contents></Contents>
+            <GlobalContents></GlobalContents>
+            <UsaContents></UsaContents>
+            <UkContents></UkContents>
+          </>
+        )
+      })
+    }
+  },[Value])
+
   const [ modalOpen, setModalOpen ] = useState(false);
 
   const openModal = () => {
@@ -39,17 +79,28 @@ function App() {
   }
   return (
   <section className="section">
-  <Header></Header>
+    <header className="header">
+    <div className="header_h1">
+    <h1>COVID-19</h1>
+    </div>
+    <select name="select" id="GraphData" onChange={(e)=>{
+        const selectvalue = e.target.value
+        setValue(selectvalue)
+    }}>
+        <option value="all">전체</option>
+        <option value="kr">국내</option>
+        <option value="global" >세계</option>
+        <option value="usa" >미국</option>
+        <option value="uk">영국</option>
+    </select>
+</header>
   <div className="DarkModeBtn_flex">
   <div className="DarkModeBtn">
   <button onClick={darkOnOff} style={{marginBottom:"20px"}}>on/off darkMode</button>
   <button onClick={openModal}>국내 지역별 거리두기</button>
   </div>
   </div>
-  <Contents></Contents>
-  <GlobalContents></GlobalContents>
-  <UsaContents></UsaContents>
-  <UkContents></UkContents>
+  {component}
   <Modal open={modalOpen} close={closeModal}>
   <Keepdistance></Keepdistance>
   </Modal>
