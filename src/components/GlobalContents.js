d6c36d3bb10e465abdcc66fb648488060f9f7562
MinBeomgi <alsqjarlwkd@naver.com>
2021-07-09 19:40:17 +0900

    세번 리렌더링 되던 부분 수정
    세번 리렌더링 되던 부분 수정

--- src/components/GlobalContents.js
+++ src/components/GlobalContents.js
@@ -5,9 +5,8 @@ import Loading from './loading';
 import UpArrow from './../imgs/up-arrow.png';
 
 const AboradContents = () => {
-
     const [Globalconfiremd,setGlobalconfiremd] = useState({})
-    const[loadingData,setloadingData] = useState(true);
+    const [loadingData,setloadingData] = useState(true);
     const [GlobalNewConfirmed,setGlobalNewConfirmed] = useState();
     const [GlobalNewDeaths,setGlobalNewDeaths] = useState();
     const [GlobalNewRecover,setGlobalNewRecover] =useState();
@@ -25,19 +24,20 @@ const AboradContents = () => {
         setGlobalNewDeaths(Global.data.Global.NewDeaths);
         setGlobalNewRecover(Global.data.Global.NewRecovered);
         setGlobalconfiremd({
-        labels:["전체 확진자","전체 사망자수","전체 격리해제"],
-        datasets:[
-            {
-                label:"전세계 누적 확진,전세계 해제,전세계 사망 비율",
-                backgroundColor:["green","yellow","red"],
-                fill:false,
-                data:[GlobalConfirmed,GlobalDeaths,GlobalRecover],
-            }
-        ]
-    })
-}
+                labels:["전체 확진자","전체 사망자수","전체 격리해제"],
+                datasets:[
+                    {
+                        label:"전세계 누적 확진,전세계 해제,전세계 사망 비율",
+                        backgroundColor:["green","yellow","red"],
+                        fill:false,
+                        data:[Global.data.Global.TotalConfirmed,Global.data.Global.TotalDeaths,Global.data.Global.TotalRecovered]
+                    }
+                ]
+            })
+    }
     fetchGlobalEvents();
-    }, [GlobalConfirmed,GlobalDeaths,GlobalRecover])
+    }, [])
+
     return (
         <>
         {loadingData ? <Loading></Loading> : 
